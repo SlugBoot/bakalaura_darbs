e18d852286798e5f4e6f329bcd3ef109edb2b198
Ēvalds Zemturis <evalds43@gmail.com>
2026-03-03 12:08:42 +0200

    Izmaiņas Person klasē
    Id tips nomainīts uz UUID
    Pievienots RegEx (pārbaudīt darbību, kad izveidots repo)
    Izveidoti argumenta konstruktori
    Pievienota mantošana (katrai klasei sava tabula TABLE_PER_CLASS)

--- learning_platform/src/main/java/lv/slugboot/app/models/Person.java
+++ learning_platform/src/main/java/lv/slugboot/app/models/Person.java
@@ -1,41 +1,60 @@
 package lv.slugboot.app.models;
 
+import java.util.UUID;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@ToString
 @Entity
-@Data
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Table(name="PersonTable")
 public class Person {
-	
+	// TODO: Pārbaudīt RegEx darbību, kad izveidots repo
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotNull
+	@GeneratedValue(strategy = GenerationType.UUID)
 	@Setter(value=AccessLevel.NONE)
-	private long id;
+	private UUID person_id;
 	
-    // TODO: Izveidot RegEx vārdam
 	@Column(name="FirstName")
-	@Pattern(regexp="")
+	@Pattern(regexp="[[:upper:]]([[:alpha:]]1-44)")
 	@NotNull
 	private String name;
 	
-	// TODO: Izveidot RegEx uzvārdam
+	@Column(name="MiddleName")
+	@Pattern(regexp="[[:upper:]]([[:alpha:]]1-44)")
+	private String middleName;
+	
 	@Column(name="LastName")
-	@Pattern(regexp="")
+	@Pattern(regexp="[[:upper:]]([[:alpha:]]1-44)")
 	@NotNull
 	private String surname;
 	
-	
+	public Person(String name, String surname) {
+		setName(name);
+		setSurname(surname);
+	}
+	public Person(String name, String middleName, String surname) {
+		setName(name);
+		setMiddleName(middleName);
+		setSurname(surname);
+	}
 }
