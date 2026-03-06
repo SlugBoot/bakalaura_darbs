f02fa3ecb9c586e863e72e72e4e80e14a82e793e
Ēvalds Zemturis <evalds43@gmail.com>
2026-03-06 14:19:03 +0200

    Izveidotas 2 jaunas modeļu klases
    2 modeļu klases:
    - Course
    - Grade

--- learning_platform/src/main/java/lv/slugboot/app/models/Course.java
+++ learning_platform/src/main/java/lv/slugboot/app/models/Course.java
@@ -1,13 +1,14 @@
 package lv.slugboot.app.models;
 
-import java.util.Collection;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -25,13 +26,34 @@ import lombok.ToString;
 public class Course {
 	
 	// TODO: Papildināt kursa klasi
-	
-	@OneToMany(mappedBy="course")
-	private Collection<Student> student;
-	
 	@Id
 	@NotNull
 	@Setter(value=AccessLevel.NONE)
 	@GeneratedValue(strategy=GenerationType.UUID)
+	@Column(name="CourseId")
 	private UUID cId;
+	
+	@NotNull
+	@Column(name="CourseName")
+	private String courseName;
+	
+	@Column(name="CourseDescription")
+	private String courseDesc;
+	
+	@NotNull
+	@OneToOne
+	@JoinColumn(name="ProfessorId")
+	private Professor professor;
+	
+	public Course(String courseName, Professor professor) {
+		setCourseName(courseName);
+		setProfessor(professor);
+	}
+	
+	public Course(String courseName,String courseDesc, Professor professor) {
+		setCourseName(courseName);
+		setCourseDesc(courseDesc);
+		setProfessor(professor);
+	}
+	
 }
---
+++ learning_platform/src/main/java/lv/slugboot/app/models/Grade.java
@@ -0,0 +1,49 @@
+package lv.slugboot.app.models;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name="GradeTable")
+public class Grade {
+
+	@Id
+	@Setter(value=AccessLevel.NONE)
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private UUID gradeId;
+	
+	@Column(name="GradeValue")
+	private int gradeValue;
+	
+	@ManyToOne
+	@JoinColumn(name="StudentPersonId")
+	private Student student;
+	
+	@ManyToOne
+	@JoinColumn(name="CourseId")
+	private Course course;
+	
+	public Grade(int gradeValue, Student student, Course course) {
+		setGradeValue(gradeValue);
+		setStudent(student);
+		setCourse(course);
+	}
+	
+}
