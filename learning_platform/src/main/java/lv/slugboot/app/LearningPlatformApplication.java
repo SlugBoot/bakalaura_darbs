b8b38bb4168d3063b2e9ddd4d97028fc22dbeef1
Ēvalds Zemturis <evalds43@gmail.com>
2026-03-06 14:16:17 +0200

    Pārbaudīta datubāzes datu ievade
    Izveidots viens students un divi profesori

--- learning_platform/src/main/java/lv/slugboot/app/LearningPlatformApplication.java
+++ learning_platform/src/main/java/lv/slugboot/app/LearningPlatformApplication.java
@@ -1,7 +1,14 @@
 package lv.slugboot.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lv.slugboot.app.models.Professor;
+import lv.slugboot.app.models.Student;
+import lv.slugboot.app.repo.IProfessorRepo;
+import lv.slugboot.app.repo.IStudentRepo;
 
 @SpringBootApplication
 public class LearningPlatformApplication {
@@ -9,5 +16,18 @@ public class LearningPlatformApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearningPlatformApplication.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner testDB(IStudentRepo studentRepo, IProfessorRepo professorRepo) {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				
+				studentRepo.save(new Student("Jānis", "Pēteris", "Bērziņš", "email@email.com"));
+				professorRepo.save(new Professor("Anna", "Felicita", "Fabriciusa", "words@emails.lv"));
+				professorRepo.save(new Professor("Andris", "Petrograds", "andris@emails.lv"));
+				System.out.println("Command Line check");
+			}
+		};
+	}
 }
